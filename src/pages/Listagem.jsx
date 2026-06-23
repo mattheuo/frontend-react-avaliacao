48d02cb403bb2d2adface66676d831ccff3c552b
Mattheus Patricio <mattheus.matos@iesb.edu.br>
2026-06-22 21:22:45 -0300

    feat: implementar listagem dinâmica de usuários

--- src/pages/Listagem.jsx
+++ src/pages/Listagem.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import { buscarUsuarios } from "../services/api";
+
 function Listagem() {
+  const [usuarios, setUsuarios] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function carregarUsuarios() {
+      try {
+        const dados = await buscarUsuarios();
+        setUsuarios(dados);
+      } catch (erro) {
+        console.error("Erro:", erro);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    carregarUsuarios();
+  }, []);
+
+  if (loading) {
+    return <h2>Carregando...</h2>;
+  }
+
   return (
     <div>
-      <h1>Listagem</h1>
+      <h1>Lista de Usuários</h1>
+
+      {usuarios.map((usuario) => (
+        <div key={usuario.id}>
+          <h3>{usuario.name}</h3>
+          <p>Email: {usuario.email}</p>
+          <p>Cidade: {usuario.address.city}</p>
+        </div>
+      ))}
     </div>
   );
 }
