ddd64a152c70cf9db628408918a77e991af2eac8
Mattheus Patricio <mattheus.matos@iesb.edu.br>
2026-06-22 21:11:57 -0300

    feat: criar páginas Home e Listagem

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <p>Bem-vindo ao projeto React.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Listagem.jsx
@@ -0,0 +1,9 @@
+function Listagem() {
+  return (
+    <div>
+      <h1>Listagem</h1>
+    </div>
+  );
+}
+
+export default Listagem;
